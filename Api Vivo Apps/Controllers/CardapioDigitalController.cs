362a09aaee580019af55efa45200308dab07a350
JHONNY GLAUCIO NASCIMENTO DE MELO <jhonny.melo@telefonica.com>
2024-06-20 19:55:02 -0300

    - Criado a funcionalidade de adicionar e atualizar informações de Produtos no Cardapio digital;

--- Api Vivo Apps/Controllers/CardapioDigitalController.cs
+++ Api Vivo Apps/Controllers/CardapioDigitalController.cs
@@ -16,6 +16,8 @@ using Shared_Static_Class.Models;
 using Shared_Static_Class.Converters;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Drawing;
+using NuGet.Versioning;
 
 namespace Vivo_Apps_API.Controllers
 {
@@ -58,7 +60,7 @@ namespace Vivo_Apps_API.Controllers
             {
                 var result = BD.PRODUTOS_CARDAPIO
                     .Include(x => x.Ficha)
-                    .Include(x => x.Imagens.Take(1))
+                    .Include(x => x.Imagens.First())
                     .AsEnumerable();
 
                 return new JsonResult(new Response<IEnumerable<PRODUTOS_CARDAPIO>>
@@ -68,7 +70,282 @@ namespace Vivo_Apps_API.Controllers
                     Message = "Você está habilitado a solicitação de acesso a ferramenta."
                 }, new JsonSerializerOptions
                 {
-                    ReferenceHandler = ReferenceHandler.IgnoreCycles 
+                    ReferenceHandler = ReferenceHandler.IgnoreCycles
+                });
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new Response<string>
+                {
+                    Data = "Recebemos a solicitação da ação mas não conseguimos executa-lá",
+                    Succeeded = false,
+                    Message = "Recebemos a solicitação da ação mas não conseguimos executa-lá",
+                    Errors = new string[]
+                    {
+                        ex.Message,
+                        ex.StackTrace
+                    },
+                });
+            }
+        }
+
+        [HttpGet("Get/{id}")]
+        [ProducesResponseType(typeof(Response<PRODUTOS_CARDAPIO>), 200)]
+        [ProducesResponseType(typeof(Response<string>), 500)]
+        public JsonResult Get(Guid id)
+        {
+            try
+            {
+                var result = BD.PRODUTOS_CARDAPIO
+                    .Include(x => x.Ficha)
+                    .First(x => x.ID_PRODUTO == id);
+
+                var countimages = BD.PRODUTO_IMAGENS
+                    .Where(x => x.ID_PRODUTO == id)
+                    .Select(x => x.ID_IMAGEM).ToArray();
+
+                for (var i = 0; i < countimages.Count(); i++)
+                {
+                    result.Imagens.Add(new PRODUTO_IMAGEM
+                    {
+                        Imagem = null,
+                        ID_IMAGEM = countimages[i],
+                        ID_PRODUTO = id
+                    });
+                }
+
+                return new JsonResult(new Response<PRODUTOS_CARDAPIO>
+                {
+                    Data = result,
+                    Succeeded = true,
+                    Message = "Você está habilitado a solicitação de acesso a ferramenta."
+                }, new JsonSerializerOptions
+                {
+                    ReferenceHandler = ReferenceHandler.IgnoreCycles
+                });
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new Response<string>
+                {
+                    Data = "Recebemos a solicitação da ação mas não conseguimos executa-lá",
+                    Succeeded = false,
+                    Message = "Recebemos a solicitação da ação mas não conseguimos executa-lá",
+                    Errors = new string[]
+                    {
+                        ex.Message,
+                        ex.StackTrace
+                    },
+                });
+            }
+        }
+
+        [HttpGet("Image/{idImage}")]
+        [ProducesResponseType(typeof(Response<PRODUTO_IMAGEM>), 200)]
+        [ProducesResponseType(typeof(Response<string>), 500)]
+        public IActionResult GetImages(Guid idImage)
+        {
+            var result = BD.PRODUTO_IMAGENS.Find(idImage);
+
+            return Ok(result);
+        }
+
+
+        [HttpDelete("Delete/{id}")]
+        [ProducesResponseType(typeof(Response<string>), 200)]
+        [ProducesResponseType(typeof(Response<string>), 500)]
+        public JsonResult Delete([FromBody] Guid id)
+        {
+            try
+            {
+                var produto = BD.PRODUTOS_CARDAPIO.Find(id);
+                var result = BD.PRODUTOS_CARDAPIO.Remove(produto);
+                BD.SaveChangesAsync();
+
+                return new JsonResult(new Response<string>
+                {
+                    Data = "Tudo certo",
+                    Succeeded = true,
+                    Message = $"O Produto {produto.Nome} foi adicionado"
+                }, new JsonSerializerOptions
+                {
+                    ReferenceHandler = ReferenceHandler.IgnoreCycles
+                });
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new Response<string>
+                {
+                    Data = "Recebemos a solicitação da ação mas não conseguimos executa-lá",
+                    Succeeded = false,
+                    Message = "Recebemos a solicitação da ação mas não conseguimos executa-lá",
+                    Errors = new string[]
+                    {
+                        ex.Message,
+                        ex.StackTrace
+                    },
+                });
+            }
+        }
+
+        [HttpPost("Post")]
+        [ProducesResponseType(typeof(Response<PRODUTOS_CARDAPIO>), 200)]
+        [ProducesResponseType(typeof(Response<string>), 500)]
+        public JsonResult Post([FromBody] PRODUTOS_CARDAPIO produto)
+        {
+            try
+            {
+                var result = BD.PRODUTOS_CARDAPIO.Add(produto);
+                BD.SaveChangesAsync();
+
+                return new JsonResult(new Response<PRODUTOS_CARDAPIO>
+                {
+                    Data = produto,
+                    Succeeded = true,
+                    Message = $"O Produto {produto.Nome} foi adicionado"
+                }, new JsonSerializerOptions
+                {
+                    ReferenceHandler = ReferenceHandler.IgnoreCycles
+                });
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new Response<string>
+                {
+                    Data = "Recebemos a solicitação da ação mas não conseguimos executa-lá",
+                    Succeeded = false,
+                    Message = "Recebemos a solicitação da ação mas não conseguimos executa-lá",
+                    Errors = new string[]
+                    {
+                        ex.Message,
+                        ex.StackTrace
+                    },
+                });
+            }
+        }
+
+        [HttpPost("Post/Image")]
+        [ProducesResponseType(typeof(Response<string>), 200)]
+        [ProducesResponseType(typeof(Response<string>), 500)]
+        public JsonResult Post([FromBody] PRODUTO_IMAGEM imagem)
+        {
+            try
+            {
+                var result = BD.PRODUTO_IMAGENS.Add(imagem);
+                BD.SaveChangesAsync();
+
+                return new JsonResult(new Response<string>
+                {
+                    Data = $"Tudo Certo",
+                    Succeeded = true,
+                    Message = "Você está habilitado a solicitação de acesso a ferramenta."
+                }, new JsonSerializerOptions
+                {
+                    ReferenceHandler = ReferenceHandler.IgnoreCycles
+                });
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new Response<string>
+                {
+                    Data = "Recebemos a solicitação da ação mas não conseguimos executa-lá",
+                    Succeeded = false,
+                    Message = "Recebemos a solicitação da ação mas não conseguimos executa-lá",
+                    Errors = new string[]
+                    {
+                        ex.Message,
+                        ex.StackTrace
+                    },
+                });
+            }
+        }
+
+        [HttpPut("Put")]
+        [ProducesResponseType(typeof(Response<PRODUTOS_CARDAPIO>), 200)]
+        [ProducesResponseType(typeof(Response<string>), 500)]
+        public JsonResult Put([FromBody] PRODUTOS_CARDAPIO produto)
+        {
+            try
+            {
+                var result = BD.PRODUTOS_CARDAPIO
+                    .Include(x => x.Ficha).First(x => x.ID_PRODUTO == produto.ID_PRODUTO);
+
+                result.Nome = produto.Nome;
+                result.Descrição = produto.Descrição;
+                result.Avaliacao = produto.Avaliacao;
+                result.Categoria = produto.Categoria;
+                result.Fabricante = produto.Fabricante;
+                result.Cor = produto.Cor;
+                result.IsOferta = produto.IsOferta;
+                result.Valor = produto.Valor;
+                result.MaxParcelas = produto.MaxParcelas;
+                result.MaxParcelasSemJuros = produto.MaxParcelasSemJuros;
+
+                IEnumerable<FICHA_TECNICA> NovaFicha = result.Ficha.Union(produto.Ficha).ToList();
+                /** Une os perfis que estão no banco e os inseridos pelo usuário em uma lista, dá o distinct automaticamente **/
+                IEnumerable<FICHA_TECNICA> FichaExcluida = result.Ficha.ExceptBy(produto.Ficha.Select(x => x.ID_FICHA), x => x.ID_FICHA).ToList();
+                /** Perfis que estão no banco e que não estão na união entre as 2 listas **/
+
+                foreach (var ficha in NovaFicha)
+                {
+                    if (ficha.ID_FICHA == Guid.Empty)
+                    {
+                        BD.FICHA_TECNICA.Add(ficha);
+                    }
+                }
+
+                foreach (var ficha in FichaExcluida)
+                {
+                    BD.FICHA_TECNICA.Remove(ficha);
+                }
+
+                var saida = BD.SaveChanges();
+
+                return new JsonResult(new Response<PRODUTOS_CARDAPIO>
+                {
+                    Data = produto,
+                    Succeeded = true,
+                    Message = $"Os dados do produto {result.Nome} foi atualizado"
+                }, new JsonSerializerOptions
+                {
+                    ReferenceHandler = ReferenceHandler.IgnoreCycles
+                });
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new Response<string>
+                {
+                    Data = "Recebemos a solicitação da ação mas não conseguimos executa-lá",
+                    Succeeded = false,
+                    Message = "Recebemos a solicitação da ação mas não conseguimos executa-lá",
+                    Errors = new string[]
+                    {
+                        ex.Message,
+                        ex.StackTrace
+                    },
+                });
+            }
+        }
+
+        [HttpDelete("Delete/Image")]
+        [ProducesResponseType(typeof(Response<string>), 200)]
+        [ProducesResponseType(typeof(Response<string>), 500)]
+        public JsonResult Delete([FromBody] PRODUTO_IMAGEM imagem)
+        {
+            try
+            {
+                BD.PRODUTO_IMAGENS.Remove(imagem);
+
+                BD.SaveChanges();
+
+                return new JsonResult(new Response<string>
+                {
+                    Data = $"Tudo Certo",
+                    Succeeded = true,
+                    Message = "Você está habilitado a solicitação de acesso a ferramenta."
+                }, new JsonSerializerOptions
+                {
+                    ReferenceHandler = ReferenceHandler.IgnoreCycles
                 });
             }
             catch (Exception ex)
